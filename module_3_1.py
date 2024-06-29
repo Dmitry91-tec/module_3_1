592361e3f0387dfa1376ed0a4fccfa8610ff4684
Dmitry <nasdi91@yandex.ru>
2024-06-29 11:25:01 +1000

    Устранены замечания

--- module_3_1.py
+++ module_3_1.py
@@ -2,25 +2,26 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
-    return calls-1
+    return
 
 def string_info(name_str):
+    count_calls()
     length = str(len(name_str))
     upp = name_str.upper()
     low = name_str.lower()
-    count_calls()
-    result = '(' + length + ",'" + upp + "', '" + low + "'" + ')'
+    result = (int(length), upp, low)
     return result
 
 def is_contains(name_str1, list_to_search):
+    count_calls()
     for i in list_to_search:                                        #поэлементно принимает i значения из списка
         if i.lower() == name_str1.lower():                          #переводим к нижнему регистру сравнеиваемые слова
             a = True
             break
         else:
             a = False
-    count_calls()
     return a
+
 
 
 print(string_info('Capybara'))
